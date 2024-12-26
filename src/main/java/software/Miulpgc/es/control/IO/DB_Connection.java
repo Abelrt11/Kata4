9cee0f0c01d40fe38d264b9d326e5fd8d2d1f8b6
Abel <abel.romerotavio@gmail.com>
2024-12-26 13:08:26 +0000

    New IO structure to read in a new way the data from titleRatings.

---
+++ src/main/java/software/Miulpgc/es/control/IO/DB_Connection.java
@@ -0,0 +1,13 @@
+package software.Miulpgc.es.control.IO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB_Connection {
+    private static final String DB_URL = "jdbc:sqlite:TitleRatings.sqlite";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+}
---
+++ src/main/java/software/Miulpgc/es/control/IO/DB_dataReader.java
@@ -0,0 +1,51 @@
+package software.Miulpgc.es.control.IO;
+
+import software.Miulpgc.es.model.DataRegister;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DB_dataReader {
+
+    public List<DataRegister> readData() {
+        String query = "SELECT * FROM title_ratings_with_id WHERE id = ?";
+
+        int currentId = 1;
+        boolean moreRows = true;
+        List<DataRegister> RegistersList = new ArrayList<>();
+        String id;
+        String averageRating;
+        String numVotes;
+        try (Connection connection = DB_Connection.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            while (moreRows) {
+                preparedStatement.setInt(1, currentId);
+                currentId++;
+
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        id = resultSet.getString("id");
+                        averageRating = resultSet.getString("average_rating");
+                        numVotes = resultSet.getString("num_votes");
+                        RegistersList.add(new DataRegister(id, Double.valueOf(averageRating), Integer.parseInt(numVotes)));
+                    } else {
+                        moreRows = false;
+                    }
+                }
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+        return RegistersList;
+    }
+
+
+}
